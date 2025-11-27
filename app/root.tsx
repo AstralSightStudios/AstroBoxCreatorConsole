f3a8a3df0573682808214112bc16cc6cbc45969c
Searchstars <searchstars@163.com>
2025-11-28 00:02:39 +0800

    b

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,21 @@
 import "./app.css";
+import "@radix-ui/themes/styles.css";
 import PageTransition from "./components/transition/page-transition";
 import Nav from "./layout/nav";
+import { Theme } from "@radix-ui/themes";
 
 export default function RootLayout() {
     return (
-        <div className="flex flex-row min-h-screen">
-            <div className="shrink-0">
-                <Nav />
-            </div>
+        <Theme appearance="dark">
+            <div className="flex flex-row min-h-screen">
+                <div className="shrink-0">
+                    <Nav />
+                </div>
 
-            <main className="flex-1">
-                <PageTransition />
-            </main>
-        </div>
+                <main className="flex-1">
+                    <PageTransition />
+                </main>
+            </div>
+        </Theme>
     );
 }
